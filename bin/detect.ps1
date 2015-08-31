bc19e3c6bd3c5c5ad842407ed3b12945302f9620
Florin Dragos <florin.dragos@hp.com>
2015-08-31 12:53:45 +0300

    Add buildpack

---
+++ bin/detect.ps1
@@ -0,0 +1,14 @@
+﻿$path = $args[0]
+
+$files = @(Get-ChildItem $path -Name)
+$solutionFile = Get-ChildItem (Join-Path $path '*.sln')
+
+IF (($files -contains "web.config") -or ($solutionFile -ne $null))
+{
+  Write-Output "IIS8-Builder"
+  exit 0
+}
+ELSE
+{
+  exit 1
+}
